441c2867f0a49200f15d29d343ae265f65287ca2
Florian Assmuth <florianassmuth@gmail.com>
2025-06-04 15:07:18 +0200

    force slurm to use cpu with all cores.

--- codes_/one_to_run_them_all.sh
+++ codes_/one_to_run_them_all.sh
@@ -3,6 +3,7 @@
 #SBATCH --error=slurm/%x_%j.err 
 #SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00 
+#SBATCH --nodelist=pgi15-cpu2
 #SBATCH --nodes=1
 #SBATCH --mail-type=END,FAIL 
 #SBATCH --mail-user=f.assmuth@fz-juelich.de
