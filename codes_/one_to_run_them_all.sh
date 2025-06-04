bccc863315bde3b3e4c036a7d4a020712ebd7d4a
Florian Assmuth <florianassmuth@gmail.com>
2025-06-04 14:21:37 +0200

    corrected and simplified sbatch script

--- codes_/one_to_run_them_all.sh
+++ codes_/one_to_run_them_all.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 #SBATCH --output=slurm/%x_%j.out 
 #SBATCH --error=slurm/%x_%j.err 
-#SBATCH --gres=gpu:4 
+#SBATCH --gres=gpu:4
 #SBATCH --time=48:00:00 
 #SBATCH --nodes=1 
 #SBATCH --mail-type=END,FAIL 
 #SBATCH --mail-user=f.assmuth@fz-juelich.de
 #SBATCH --job-name=dANN
 
-srun run_all_depth_test.sh
-srun run_all_depth.sh
-srun run_all_drop.sh
-srun run_all_early_stop.sh
-srun run_all_fig2.sh
-srun run_all_fig3.sh
-srun run_all_fig6B.sh
-srun run_all_fig6D.sh
-srun run_all_lr.sh
-srun run_analysis_eval_depth.sh
-srun run_analysis_eval_dropout.sh
-srun run_analysis_eval_learn_rate.sh
-srun run_analysis_eval.sh
+for x in $(ls run*.sh); do sh x; end
