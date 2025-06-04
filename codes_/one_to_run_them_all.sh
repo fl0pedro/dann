b95b7399d5e85482f8b1153e03ea9819aaec42b8
Florian Assmuth <florianassmuth@gmail.com>
2025-06-04 15:02:50 +0200

    switching to cpu GPUs not being used by the script...

--- codes_/one_to_run_them_all.sh
+++ codes_/one_to_run_them_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --output=slurm/%x_%j.out 
 #SBATCH --error=slurm/%x_%j.err 
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00 
-#SBATCH --nodes=1 
+#SBATCH --nodes=1
 #SBATCH --mail-type=END,FAIL 
 #SBATCH --mail-user=f.assmuth@fz-juelich.de
 #SBATCH --job-name=dANN
