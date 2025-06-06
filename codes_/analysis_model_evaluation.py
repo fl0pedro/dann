3f7b78fc57d6f934859bfc5e5dcce5aad32bf433
Florian Assmuth <florianassmuth@gmail.com>
2025-06-06 08:13:53 +0200

    make both scripts correct __main__ scripts as well as callable main functions

--- codes_/analysis_model_evaluation.py
+++ codes_/analysis_model_evaluation.py
@@ -1,195 +1,131 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 23 09:24:53 2023
-
-@author: spiros
+Created on Tue May 18 12:00:15 2021.
+@author: spiros and assmuth
 """
 import os
-import sys
 import time
 import pickle
 import pathlib
 import numpy as np
 import pandas as pd
+import argparse
+from itertools import product
 
-from utils import num_trainable_params
-from utils import get_power_of_10
+from utils import num_trainable_params, get_power_of_10
 
-t1 = time.time()
+def parse_args(args: list[str] | None = None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir_full_path", type=str)
+    parser.add_argument("datatype", choices=["mnist", "fmnist", "kmnist", "emnist", "cifar10"])
+    parser.add_argument("--sequential", action="store_true")
+    parser.add_argument("--noise", action="store_true")
+    parser.add_argument("--dropout", action="store_true")
+    parser.add_argument("--num-layers", type=int, required=True)
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--early-stop", action="store_true")
+    parser.add_argument("--save", action="store_true", default=True)
+    return parser.parse_args(args)
 
-# Simulation parameters
-dir_full_path = sys.argv[1]
-datatype = sys.argv[2]  # datatype (mnist, fmnist, kmnist, emnist, cifar10)
-save_data = True
-seq = int(sys.argv[3])
-noise = int(sys.argv[4])
-drop = int(sys.argv[5])
-num_layers = int(sys.argv[6])
-lr = float(sys.argv[7])
-early_stop_flag = int(sys.argv[8])
+def main(*args):
+    args = parse_args(args)
+    print(args)
+    t1 = time.time()
 
-if drop == 0:
-    models = [
-        'dend_ann_random',
-        'dend_ann_local_rfs',
-        'dend_ann_global_rfs',
-        'dend_ann_all_to_all',
-        'vanilla_ann',
-        'vanilla_ann_local_rfs',
-        'vanilla_ann_global_rfs',
-        'vanilla_ann_random',
-        'sparse_ann',
-        'sparse_ann_local_rfs',
-        'sparse_ann_global_rfs',
-        'sparse_ann_all_to_all',
-    ]
-elif drop == 1:
-    models = [
-        'dend_ann_random',
-        'dend_ann_local_rfs',
-        'dend_ann_global_rfs',
-        'vanilla_ann',
-        'vanilla_ann_dropout_0.2',
-        'vanilla_ann_dropout_0.5',
-        'vanilla_ann_dropout_0.8',
-    ]
-
-trials = 5
-
-somata = [2**i for i in range(5, 10)]  # number of somata (i.e., somatic, second layer)
-dendrites = [2**i for i in range(7)]  # number of dendrites per soma (i.e., dendritic layer)
-
-tag = "" if seq == 0 else "_sequential"
-tag2 = "" if lr == 1e-3 else f"_lr_{lr}"
-sigmas = [0.0] if noise == 0 else [0.0, 0.25, 0.5, 0.75, 1.0]
-
-# Hyperparameters
-if datatype == 'mnist':
-    n_epochs = 15 if not seq else 30
-elif datatype == 'fmnist' or datatype == 'kmnist':
-    n_epochs = 25 if not seq else 50
-elif datatype == 'cifar10' or datatype == 'emnist':
-    n_epochs = 50
-
-if early_stop_flag == 1:
-    n_epochs = 100
-
-# data frame to store all analysis for training, validation and test
-print(f"\nresults_{datatype}_{num_layers}_layer{tag}{tag2}/")
-
-df_all = None
-df_test = None
-for model_type in models:
-    for sigma in sigmas:
-        for num_soma in somata:
-            for num_dends in dendrites:
-                for t in range(1, trials+1):
-                    print(f"Analyze model: {model_type} with {num_soma} somata"
-                          f" and {num_dends} dendrites for trials {t}, sigma {sigma}")
-                    # Set up directory and file names
-                    dirname = f"{dir_full_path}/results_{datatype}_{num_layers}_layer{tag}{tag2}/"
-                    subdir = f"{model_type}"
-                    postfix = f"sigma_{sigma}_trial_{t}_dends_{num_dends}_soma_{num_soma}"
-                    fname_data = pathlib.Path(f"{dirname}/{subdir}/results_{postfix}.pkl")
-
-                    # load the training/validation results
-                    if not os.path.isfile(fname_data):
-                        continue
-
-                    with open(fname_data, 'rb') as file:
-                        DATA = pickle.load(file)
-
-                    trainable_count = num_trainable_params(
-                        D=num_dends,
-                        S=num_soma,
-                        model_type=model_type
-                    )
-                    # remove the key 'Masks'
-                    if 'Masks' in DATA.keys():
-                        DATA.pop('Masks')
-                    # Create the DataFrame
-                    if df_all is None:
-                        df_all = pd.DataFrame.from_dict(DATA)
-                        df_all['trial'] = t
-                        df_all['model'] = model_type
-                        df_all['epoch'] = range(1, len(df_all)+1)
-                        df_all['sigma'] = sigma
-                        df_all['num_dends'] = num_dends
-                        df_all['num_soma'] = num_soma
-                        df_all['trainable_params'] = trainable_count
-                        df_all['trainable_params_grouped'] = 10**get_power_of_10(trainable_count)
-                    else:
-                        df_ = pd.DataFrame.from_dict(DATA)
-                        df_['trial'] = t
-                        df_['model'] = model_type
-                        df_['epoch'] = range(1, len(df_)+1)
-                        df_['sigma'] = sigma
-                        df_['num_dends'] = num_dends
-                        df_['num_soma'] = num_soma
-                        df_['trainable_params'] = trainable_count
-                        df_['trainable_params_grouped'] = 10**get_power_of_10(trainable_count)
-                        df_all = pd.concat([df_all, df_], ignore_index=True)
-
-                    # Create the DataFrame
-                    if df_test is None:
-                        df_test = pd.DataFrame(
-                            columns=['test_acc',
-                                     'test_loss',
-                                     'model',
-                                     'num_dends',
-                                     'num_soma',
-                                     'sigma',
-                                     'trial'],
-                            index=[0]
-                            )
-                        df_test['test_acc'] = DATA['test_acc']
-                        df_test['test_loss'] = DATA['test_loss']
-                        df_test['trial'] = t
-                        df_test['model'] = model_type
-                        df_test['sigma'] = sigma
-                        df_test['num_dends'] = num_dends
-                        df_test['num_soma'] = num_soma
-                        df_test['trainable_params'] = trainable_count
-                        df_test['trainable_params_grouped'] = 10**get_power_of_10(trainable_count)
-                        df_test['num_epochs_min'] = np.argmin(DATA['val_loss'])
-                    else:
-                        df_ = pd.DataFrame(
-                            columns=['test_acc',
-                                     'test_loss',
-                                     'model',
-                                     'num_dends',
-                                     'num_soma',
-                                     'sigma',
-                                     'trial'],
-                            index=[0]
-                            )
-                        df_['test_acc'] = DATA['test_acc']
-                        df_['test_loss'] = DATA['test_loss']
-                        df_['trial'] = t
-                        df_['model'] = model_type
-                        df_['sigma'] = sigma
-                        df_['num_dends'] = num_dends
-                        df_['num_soma'] = num_soma
-                        df_['trainable_params'] = trainable_count
-                        df_['trainable_params_grouped'] = 10**get_power_of_10(trainable_count)
-                        df_['num_epochs_min'] = np.argmin(DATA['val_loss'])
-                        df_test = pd.concat([df_test, df_], ignore_index=True)
-
-if save_data:
-    results = {}
-    results['training'] = df_all
-    results['testing'] = df_test
-    # Store data (serialize)
-    if noise and not drop:
-        fname_store = pathlib.Path(f"{dirname}/output_all_noise_final.pkl")
-    elif drop and not noise:
-        fname_store = pathlib.Path(f"{dirname}/output_all_dropout_final.pkl")
+    if not args.dropout:
+        models = [
+            "dend_ann_random", "dend_ann_local_rfs", "dend_ann_global_rfs", "dend_ann_all_to_all",
+            "vanilla_ann", "vanilla_ann_local_rfs", "vanilla_ann_global_rfs", "vanilla_ann_random",
+            "sparse_ann", "sparse_ann_local_rfs", "sparse_ann_global_rfs", "sparse_ann_all_to_all",
+        ]
     else:
-        fname_store = pathlib.Path(f"{dirname}/output_all_final.pkl")
+        models = [
+            "dend_ann_random", "dend_ann_local_rfs", "dend_ann_global_rfs",
+            "vanilla_ann", "vanilla_ann_dropout_0.2", "vanilla_ann_dropout_0.5", "vanilla_ann_dropout_0.8"
+        ]
 
-    os.makedirs(dirname, exist_ok=True)
-    with open(f'{fname_store}', 'wb') as handle:
-        pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    somata = [2<<i for i in range(5, 10)]
+    dendrites = [2<<i for i in range(7)]
+    sigmas = [0.0] if not args.noise else np.linspace(0,1,5).tolist()
+    trials = range(1, 6)
+    tag = "_sequential" if args.sequential else ""
+    tag += f"_lr_{args.learning_rate}" if args.learning_rate != 1e-3 else ""
 
-print(f"Time taken to analyze and save data: {round(time.time() - t1, 2)} sec")
+    num_epochs = {
+        "mnist": 15,
+        "fmnist": 25,
+        "kmnist": 25,
+        "emnist": 50,
+        "cifar10": 50,
+    }[args.dataset]
+    if args.sequential:
+        num_epochs *= 2
+
+    if args.early_stop:
+        num_epochs = 100
+
+    df_all, df_test = pd.DataFrame(), pd.DataFrame()
+    dirname = pathlib.Path(args.dir_full_path, f"results_{args.datatype}_{args.num_layers}_layer{tag}")
+
+    for model_type, sigma, num_soma, num_dends, t in product(models, sigmas, somata, dendrites, trials):
+        fname = dirname / model_type / f"results_sigma_{sigma}_trial_{t}_dends_{num_dends}_soma_{num_soma}.pkl"
+        if not fname.is_file():
+            continue
+
+        with open(fname, "rb") as f:
+            data = pickle.load(f)
+
+        data.pop("masks", None)
+        trainable_count = num_trainable_params(D=num_dends, S=num_soma, model_type=model_type)
+
+        meta = {
+            "trial": t,
+            "model": model_type,
+            "sigma": sigma,
+            "num_dends": num_dends,
+            "num_soma": num_soma,
+            "trainable_params": trainable_count,
+            "trainable_params_grouped": 10 ** get_power_of_10(trainable_count),
+        }
+
+        df = pd.DataFrame(data)
+        df["epoch"] = range(1, len(df) + 1)
+        for k, v in meta.items():
+            df[k] = v
+
+        df_all = df_all.append(df, ignore_index=True)
+
+        df_test_entry = {
+            "test_acc": data["test_acc"],
+            "test_loss": data["test_loss"],
+            "trial": t,
+            "model": model_type,
+            "sigma": sigma,
+            "num_dends": num_dends,
+            "num_soma": num_soma,
+            "trainable_params": trainable_count,
+            "trainable_params_grouped": 10 ** get_power_of_10(trainable_count),
+            "num_epochs_min": int(np.argmin(data["val_loss"])),
+        }
+
+        df_test = df_test.append(pd.DataFrame([df_test_entry]), ignore_index=True)
+
+    if args.save:
+        results = {"training": df_all, "testing": df_test}
+        
+        if args.noise and not args.dropout:
+            fname_out = "output_all_noise_final.pkl"
+        elif args.dropout and not args.noise:
+            fname_out = "output_all_dropout_final.pkl"
+        else:
+            fname_out = "output_all_final.pkl"
+
+        os.makedirs(dirname, exist_ok=True)
+        with open(dirname / fname_out, "wb") as f:
+            pickle.dump(results, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print(f"Time: {round(time.time() - t1, 2)} sec")
+
+if __name__ == "__main__":
+    main()
--- codes_/main.py
+++ codes_/main.py
@@ -2,219 +2,197 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue May 18 12:00:15 2021.
-
-@author: spiros and florian
+@author: spiros and assmuth
 """
 import os
 import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-d", "--num-dendrites", type=int, required=True)
-parser.add_argument("-s", "--num-somas", type=int, required=True)
-parser.add_argument("-o", "--output", dest="dirname")
-parser.add_argument("--gpu", action="store_true")
-parser.add_argument("--force", action="store_true")
-parser.add_argument("--sequential", action="store_true")
-parser.add_argument("--early-stop", action="store_true")
-parser.add_argument("--all-to-all", action="store_true")
-parser.add_argument("--conventional", action="store_true")
-parser.add_argument("--sparse", action="store_true")
-parser.add_argument("--rfs", choices=["somatic", "dendritic"])
-parser.add_argument("--model", type=int)
-parser.add_argument("--trial", type=int, default=0)
-parser.add_argument("--sigma", type=float, default=0.0)
-parser.add_argument("--num-layers", type=int, default=1)
-parser.add_argument("--num-synapses", dest="nsyns", type=int, default=16)
-parser.add_argument("--drop-rate", type=float, default=0)
-parser.add_argument("--learning-rate", dest="lr", type=float, default=1e-3)
-parser.add_argument("--dataset", choices=["mnist", "fmnist", "kmnist", "emnist", "cifar10"], default="fmnist")
-parser.add_argument("--backend", choices=["tensorflow", "torch", "jax"], default="jax")
-
-args = parser.parse_args()
-
-print(args)
-
-os.environ["KERAS_BACKEND"] = args.backend
-os.environ["CUDA_VISIBLE_DEVICES"] = str(int(args.gpu))
-
-import copy
-import keras
-import pickle
 import pathlib
+import pickle
+import copy
 
-from opt import (
-    custom_train_loop_tensorflow, custom_train_loop_torch, custom_train_loop_jax, 
-    make_masks, get_model, get_data, get_model_name
-)
+def parse_args(args: list[str] | None = None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--num-dendrites", type=int, required=True)
+    parser.add_argument("-s", "--num-somas", type=int, required=True)
+    parser.add_argument("-o", "--output", dest="dirname")
+    parser.add_argument("--gpu", action="store_true")
+    parser.add_argument("--force", action="store_true")
+    parser.add_argument("--sequential", action="store_true")
+    parser.add_argument("--early-stop", action="store_true")
+    parser.add_argument("--all-to-all", action="store_true")
+    parser.add_argument("--conventional", action="store_true")
+    parser.add_argument("--sparse", action="store_true")
+    parser.add_argument("--rfs", choices=["somatic", "dendritic"])
+    parser.add_argument("--model", type=int)
+    parser.add_argument("--trial", type=int, default=0)
+    parser.add_argument("--sigma", type=float, default=0.0)
+    parser.add_argument("--num-layers", type=int, default=1)
+    parser.add_argument("--num-synapses", dest="nsyns", type=int, default=16)
+    parser.add_argument("--drop-rate", type=float, default=0)
+    parser.add_argument("--learning-rate", dest="lr", type=float, default=1e-3)
+    parser.add_argument("--dataset", choices=["mnist", "fmnist", "kmnist", "emnist", "cifar10"], default="fmnist")
+    parser.add_argument("--backend", choices=["tensorflow", "torch", "jax"], default="jax")
+    return parser.parse_args(args)
 
-run_tag = ""
-if args.sequential:
-    run_tag += "_sequential"
-if args.lr != 1e-3: # i.e., not default
-    run_tag += f"_lr_{args.lr}"
+def main(*args):
+    args = parse_args(args)
+    print(args)
 
-fname_model = get_model_name(
-    args.conventional,
-    args.rfs is not None,
-    args.sparse,
-    args.rfs,
-    "all_to_all" if args.all_to_all else None
-)
-if args.drop_rate:
-    fname_model += f"_dropout_{args.drop_rate}"
+    os.environ["KERAS_BACKEND"] = args.backend
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(int(args.gpu))
 
-print(f"\nModel: {fname_model}, trial: {args.trial}, layers: {args.num_layers}, "
-      f"noise: {args.sigma}, dataset: {args.dataset}, tag: {run_tag}\n")
+    import keras
+    from opt import (
+        custom_train_loop_tensorflow, custom_train_loop_torch, custom_train_loop_jax, 
+        make_masks, get_model, get_data, get_model_name
+    )
 
-if args.dirname:
-    subdirname = f"results_{args.dataset}_{args.num_layers}_layer{run_tag}"
-    fulldir = pathlib.Path(args.dirname, subdirname, fname_model)
+    run_tag = ""
+    if args.sequential:
+        run_tag += "_sequential"
+    if args.lr != 1e-3:
+        run_tag += f"_lr_{args.lr}"
 
-    postfix = f"sigma_{args.sigma}_trial_{args.trial}_dends_{args.num_dendrites}_soma_{args.num_somas}"
-    
-    if (fulldir / f"results_{postfix}.pkl").exists() and not args.force:
-        print(f"This run has already been recorded: {fulldir}/results_{postfix}.pkl")
-        exit(0)
+    fname_model = get_model_name(
+        args.conventional,
+        args.rfs is not None,
+        args.sparse,
+        args.rfs,
+        "all_to_all" if args.all_to_all else None
+    )
+    if args.drop_rate:
+        fname_model += f"_dropout_{args.drop_rate}"
 
+    print(f"\nModel: {fname_model}, trial: {args.trial}, layers: {args.num_layers}, "
+          f"noise: {args.sigma}, dataset: {args.dataset}, tag: {run_tag}\n")
 
-keras.utils.set_random_seed(args.trial)
+    if args.dirname:
+        subdirname = f"results_{args.dataset}_{args.num_layers}_layer{run_tag}"
+        fulldir = pathlib.Path(args.dirname, subdirname, fname_model)
 
-match args.model:
-    case 0: # dendritic ANN (dANN) with random connections
-        pass
-    case 1: # dendritic ANN (dANN) with global RFs
-        args.rfs = "somatic"
-    case 2: # dendritic ANN (dANN) with local RFs
-        args.rfs = "dendritic"
-    case 3: # vanilla ANN (vANN) all-to-all connections (no RFs)
-        args.conventional = True
-    case 4: # vanilla ANN (vANN) with random connections
-        args.conventional = True
-        args.sparse = True
-    case 5: # vanilla ANN (vANN) with global RFs
-        args.conventional = True
-        args.rfs = "somatic"
-    case 6: # vanilla ANN (vANN) with local RFs
-        args.conventional = True
-        args.rfs = "dendritic"
-    case 7: # sparse ANN (sANN)
-        args.sparse = True
-    case 8: # sparse ANN (sANN) with global RFs
-        args.rfs = "somatic"
-        args.sparse = True
-    case 9: # sparse ANN (sANN) with local RFs
-        args.rfs = "dendritic"
-        args.sparse = True
-    case 10: # dendritic ANN (dANN) with all-to-all inputs
-        args.rfs = "somatic"
-        args.all_to_all = True
-    case 11: # sparse ANN (sANN) with all-to-all inputs
-        args.conventional = False
-        args.rfs = "somatic"
-        args.sparse = True
-        args.all_to_all = True
+        postfix = f"sigma_{args.sigma}_trial_{args.trial}_dends_{args.num_dendrites}_soma_{args.num_somas}"
+        result_file = fulldir / f"results_{postfix}.pkl"
 
-batch_size = 128
-data, labels, img_height, img_width, channels = get_data(
-    validation_split=0.1,
-    dtype=args.dataset,
-    normalize=True,
-    add_noise=bool(args.sigma),
-    sigma=args.sigma,
-    sequential=args.sequential,
-    batch_size=batch_size,
-    seed=args.trial,
-)
-x_train, x_val, x_test = data["train"], data["val"], data["test"]
-y_train, y_val, y_test = labels["train"], labels["val"], labels["test"]
+        if result_file.exists() and not args.force:
+            print(f"This run has already been recorded: {result_file}")
+            return
 
-num_classes = len(set(y_train))
-dends = args.num_layers*[args.num_dendrites]
-soma = args.num_layers*[args.num_somas]
+    keras.utils.set_random_seed(args.trial)
 
+    update_model_config(args)
 
-input_shape = (img_width * img_height * channels, )
-model = get_model(
-    input_shape,
-    args.num_layers,
-    dends,
-    soma,
-    num_classes,
-    fname_model=fname_model,
-    dropout=bool(args.drop_rate),
-    rate=args.drop_rate,
-)
+    batch_size = 128
+    data, labels, img_height, img_width, channels = get_data(
+        validation_split=0.1,
+        dtype=args.dataset,
+        normalize=True,
+        add_noise=bool(args.sigma),
+        sigma=args.sigma,
+        sequential=args.sequential,
+        batch_size=batch_size,
+        seed=args.trial,
+    )
 
+    x_train, x_val, x_test = data["train"], data["val"], data["test"]
+    y_train, y_val, y_test = labels["train"], labels["val"], labels["test"]
 
-masks = make_masks(
-    dends,
-    soma,
-    args.nsyns,
-    args.num_layers,
-    img_width,
-    img_height,
-    num_classes,
-    channels,
-    conventional=args.conventional,
-    rfs=args.rfs is not None,
-    rfs_type=args.rfs,
-    rfs_mode="random",
-    seed=args.trial,
-)
+    num_classes = len(set(y_train))
+    dends = args.num_layers * [args.num_dendrites]
+    soma = args.num_layers * [args.num_somas]
 
-params = model.get_weights()
-params_masked = [params[i]*masks[i] for i in range(len(params))]
+    input_shape = (img_width * img_height * channels,)
+    model = get_model(
+        input_shape, args.num_layers, dends, soma, num_classes,
+        fname_model=fname_model, dropout=bool(args.drop_rate), rate=args.drop_rate,
+    )
 
-model.set_weights(params_masked)
-model_untrained = copy.deepcopy(model)
+    masks = make_masks(
+        dends, soma, args.nsyns, args.num_layers,
+        img_width, img_height, num_classes, channels,
+        conventional=args.conventional,
+        rfs=args.rfs is not None,
+        rfs_type=args.rfs,
+        rfs_mode="random",
+        seed=args.trial,
+    )
 
-# Instantiate the optimizer and the loss function
-optimizer = keras.optimizers.Adam(learning_rate=args.lr)
-loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+    params = model.get_weights()
+    model.set_weights([p * m for p, m in zip(params, masks)])
+    model_untrained = copy.deepcopy(model)
 
-match args.dataset:
-    case "mnist":
-        num_epochs = 15
-    case "fmnist":
-        num_epochs = 25
-    case "kmnist":
-        num_epochs = 25
-    case "emnist":
-        num_epochs = 50
-    case "cifar10":
-        num_epochs = 50
+    optimizer = keras.optimizers.Adam(learning_rate=args.lr)
+    loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=False)
 
-if args.sequential:
-    num_epochs *= 2
+    num_epochs = {
+        "mnist": 15,
+        "fmnist": 25,
+        "kmnist": 25,
+        "emnist": 50,
+        "cifar10": 50,
+    }[args.dataset]
+    if args.sequential:
+        num_epochs *= 2
+    if args.early_stop:
+        num_epochs = 100
 
-if args.early_stop:
-    num_epochs = 100
+    backend_map = {
+        "jax": custom_train_loop_jax,
+        "tensorflow": custom_train_loop_tensorflow,
+        "torch": custom_train_loop_torch,
+    }
+    train_fn = backend_map[args.backend]
 
-custom_train_loop = locals()[f"custom_train_loop_{args.backend}"]
+    model, out = train_fn(
+        model, loss_fn, optimizer, masks, batch_size, num_epochs,
+        x_train, y_train, x_val, y_val, x_test, y_test,
+        shuffle=not args.sequential, early_stop=args.early_stop, patience=10,
+    )
 
-model, out = custom_train_loop(
-    model, loss_fn, optimizer,
-    masks, batch_size,
-    num_epochs,
-    x_train, y_train,
-    x_val, y_val,
-    x_test, y_test,
-    shuffle=not args.sequential,
-    early_stop=args.early_stop,
-    patience=10,
-)
+    out["masks"] = masks
 
+    if args.dirname:
+        os.makedirs(fulldir, exist_ok=True)
+        model_untrained.save(fulldir / f"untrained_model_{postfix}.keras")
+        model.save(fulldir / f"model_{postfix}.keras")
+        with open(result_file, "wb") as f:
+            pickle.dump(out, f, protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"\nResults saved: {fulldir}/[untrained_]model_{postfix}.keras+pkl")
 
-out["masks"] = masks
+def update_model_config(args):
+    match args.model:
+        case 0:
+            pass
+        case 1:
+            args.rfs = "somatic"
+        case 2:
+            args.rfs = "dendritic"
+        case 3:
+            args.conventional = True
+        case 4:
+            args.conventional = True
+            args.sparse = True
+        case 5:
+            args.conventional = True
+            args.rfs = "somatic"
+        case 6:
+            args.conventional = True
+            args.rfs = "dendritic"
+        case 7:
+            args.sparse = True
+        case 8:
+            args.rfs = "somatic"
+            args.sparse = True
+        case 9:
+            args.rfs = "dendritic"
+            args.sparse = True
+        case 10:
+            args.rfs = "somatic"
+            args.all_to_all = True
+        case 11:
+            args.conventional = False
+            args.rfs = "somatic"
+            args.sparse = True
+            args.all_to_all = True
 
-if args.dirname:
-    os.makedirs(fulldir, exist_ok=True)
-    
-    model_untrained.save(fulldir / f"untrained_model_{postfix}.keras")
-    model.save(fulldir / f"model_{postfix}.keras")
-
-    with open(fulldir / f"results_{postfix}.pkl", "wb") as f:
-        pickle.dump(out, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-    print(f"\nResults have been saved in: {fulldir}/[untrained_]model_{postfix}.keras+pkl")
+if __name__ == "__main__":
+    main()
